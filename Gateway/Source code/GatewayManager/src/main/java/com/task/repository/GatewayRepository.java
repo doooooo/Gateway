681be28c8ec804a63b15e1021f081c3e911d5a40
doaa <doaa.farouk07@gmail.com>
2021-11-20 11:42:57 +0200

    Update GatewayRepository.java

--- Gateway/Source code/GatewayManager/src/main/java/com/task/repository/GatewayRepository.java
+++ Gateway/Source code/GatewayManager/src/main/java/com/task/repository/GatewayRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 import com.task.model.Gateway;
 
 //Dodo Repo
+//Test 2
 public interface GatewayRepository extends CrudRepository<Gateway, String> {
 
 }
